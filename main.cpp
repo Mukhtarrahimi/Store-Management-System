d4a5f5a92da09f74beeec5f8d479ba95810a4ad4
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-05-06 23:56:24 +0330

    Add private members of Customer class

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 #include <vector>
 #include <string>
 using namespace std;
+class Customer {
+private:
+    int customerID;               
+    string name;                  
+    string phone;                 
+    vector<pair<int, int>> cart;
+};
 
 
 int main() {
